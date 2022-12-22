31643479638bc0be09397f78f15736081aaf14a4
Igor-Zhelezniak-1 <90275833+Igor-Zhelezniak-1@users.noreply.github.com>
2022-12-22 09:01:05 -0500

    -

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@
  * Since:   2022-11-07
  */
 
-//process
+// Process
 function reverseString(str: string): string {
   if (str === '') {
     return str
@@ -20,7 +20,7 @@ const aString = 'recursion'
 
 // Output
 console.log()
-console.log(`The original string is: ${aString}`)
-console.log(`The reversed string is: ${reverseString(aString)}`)
+console.log(`Original: ${aString}`)
+console.log(`Reversed: ${reverseString(aString)}`)
 
 console.log('\nDone.')
